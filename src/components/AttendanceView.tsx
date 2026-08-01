b9e2ba2460a373500be8b7e9da58d2e8b7025681
mydpm-apps <mydpmapps@gmail.com>
2026-08-01 15:53:23 +0700

    style: change attendance session list layout from grid to vertical flex column

--- src/components/AttendanceView.tsx
+++ src/components/AttendanceView.tsx
@@ -896,7 +896,7 @@ export default function AttendanceView({ getToken, participants }: Props) {
               )}
             </div>
           ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="flex flex-col gap-4 max-w-3xl mx-auto w-full">
               {filteredSessions.map((session) => {
                 const isLocked = session.isPermanent === 1;
                 const canEditThis = !isLocked || isSuperAdmin;
